0fce80f03170ee279a25c05f9fa351f13f8dc321
Hanjun Kim <hallazzang@gmail.com>
2019-10-23 14:26:25 +0900

    에미터에서 보내는 멘체스터 부호화된 신호를 복호화하여 출력

--- icvlc-receiver.ino
+++ icvlc-receiver.ino
@@ -1,14 +1,37 @@
 #include <TimerOne.h>
+#include "Manchester.h"
 
+#define SYMBOL_PERIOD 3000
 #define SAMPLE_PERIOD 100
-
+#define SAMPLE_PER_SYMBOL (SYMBOL_PERIOD / SAMPLE_PERIOD)
+#define DOUBLE_SYMBOL_THRESHOLD 1.2
+#define NUM_SYMBOLS 20
 #define LEVEL_THRESHOLD 4
 
 const int sensorPin = A0;
 
-unsigned long symbols = 0;
-int state = 0;
+int state = 0, steadyCounter = 0;
 int vertexValue = 0;
+
+unsigned long symbols = 0;
+int symbolCounter;
+
+inline void pushSymbol(int symbol) {
+  symbols = (symbols << 1) | state;
+  symbolCounter++;
+  if (symbolCounter >= NUM_SYMBOLS) {
+    if (checkWord(symbols)) {
+      char ch;
+
+      if (decodeWord(symbols, &ch)) {
+        if (isAlpha(ch)) {
+          Serial.print(ch);
+          symbolCounter = 0;
+        }
+      }
+    }
+  }
+}
 
 void timerInterrupt() {
   int sensorValue = analogRead(sensorPin);
@@ -16,20 +39,27 @@ void timerInterrupt() {
   if (abs(sensorValue - vertexValue) >= LEVEL_THRESHOLD) {
     state = sensorValue < vertexValue;
 
-    symbols = (symbols << 1) | state;
-    Serial.println(symbols, BIN);
-    
+    pushSymbol(state);
+
     vertexValue = sensorValue;
+    steadyCounter = 0;
   } else {
     if ((!state && sensorValue > vertexValue) || (state && sensorValue < vertexValue)) {
       vertexValue = sensorValue;
+    }
+
+    steadyCounter++;
+
+    if (steadyCounter >= SAMPLE_PER_SYMBOL * DOUBLE_SYMBOL_THRESHOLD) {
+      pushSymbol(state);
+      steadyCounter = 0;
     }
   }
 }
 
 void setup() {
   Serial.begin(9600);
-  
+
   Timer1.initialize(SAMPLE_PERIOD);
   Timer1.attachInterrupt(timerInterrupt);
 }
