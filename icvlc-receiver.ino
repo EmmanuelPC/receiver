c14269f0d23f06f62978e421fb917f2db3c8f4a3
Hanjun Kim <hallazzang@gmail.com>
2019-10-23 13:58:21 +0900

    레벨 전이가 일어나는 지점 찾기

--- icvlc-receiver.ino
+++ icvlc-receiver.ino
@@ -1,13 +1,26 @@
 #include <TimerOne.h>
 
-#define SAMPLE_PERIOD 10000
+#define SAMPLE_PERIOD 100
+
+#define LEVEL_THRESHOLD 4
 
 const int sensorPin = A0;
+
+int state = 0;
+int vertexValue = 0;
 
 void timerInterrupt() {
   int sensorValue = analogRead(sensorPin);
 
-  Serial.println(sensorValue);
+  if (abs(sensorValue - vertexValue) >= LEVEL_THRESHOLD) {
+    state = sensorValue < vertexValue;
+    
+    vertexValue = sensorValue;
+  }
+
+  Serial.print(sensorValue);
+  Serial.print(" ");
+  Serial.println(vertexValue);
 }
 
 void setup() {
