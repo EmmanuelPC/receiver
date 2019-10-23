68dad9a3afa46a5108aeedb5025938bc89132f75
Hanjun Kim <hallazzang@gmail.com>
2019-10-23 21:47:04 +0900

    파싱 상태 머신 추가

--- icvlc-receiver.ino
+++ icvlc-receiver.ino
@@ -1,12 +1,15 @@
 #include <TimerOne.h>
 #include "Manchester.h"
 
-#define SYMBOL_PERIOD 3000
+#define SYMBOL_PERIOD 5000
 #define SAMPLE_PERIOD 100
 #define SAMPLE_PER_SYMBOL (SYMBOL_PERIOD / SAMPLE_PERIOD)
 #define DOUBLE_SYMBOL_THRESHOLD 1.2
 #define NUM_SYMBOLS 20
 #define LEVEL_THRESHOLD 4
+
+#define STX 0x02
+#define ETX 0x03
 
 #define SENSOR_PIN A0
 
@@ -16,18 +19,40 @@ int vertexValue = 0;
 unsigned long symbols = 0;
 int symbolCounter;
 
-inline void pushSymbol(int symbol) {
+enum ParsingState {
+  Idle,
+  Started,
+};
+
+ParsingState ps = Idle;
+
+void pushSymbol(int symbol) {
+  char ch;
+
   symbols = (symbols << 1) | state;
   symbolCounter++;
   if (symbolCounter >= NUM_SYMBOLS) {
-    if (checkWord(symbols)) {
-      char ch;
-
-      if (decodeWord(symbols, &ch)) {
-        if (isAlpha(ch)) {
+    if (ps == Idle) {
+      if (checkWord(symbols) && decodeWord(symbols, &ch)) {
+        if (ch == STX) {
+          ps = Started;
+          symbolCounter = 0;
+          Serial.print("^");
+        }
+      }
+    } else if (ps == Started) {
+      if (checkWord(symbols) && decodeWord(symbols, &ch)) {
+        if (ch == ETX) {
+          ps = Idle;
+          symbolCounter = 0;
+          Serial.print("$");
+        } else {
           Serial.print(ch);
           symbolCounter = 0;
         }
+      } else {
+        ps = Idle;
+        Serial.print("#");
       }
     }
   }
